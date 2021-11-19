c8637706b9286ad2ccbab917f2ef777838fb0ca8
Brian Abrahamsen <brian.abrahamsen@gmail.com>
2021-11-18 19:57:11 -0700

    More tests, more languages
    More tests, more languages
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world

--- src/hello.go
+++ src/hello.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
-const hello_and_a_comma = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const english_hello_and_a_comma = "Hello, "
+const spanish_hola_and_a_comma = "Hola, "
+const french_and_a_comma = "I Surrender, "
 
-func Hello(name string) string {
-    return hello_and_a_comma+name
+func Hello(name string, language string) string {
+
+	greeting := english_hello_and_a_comma
+
+	if name == "" {
+		name = "World"
+	}
+
+	switch language {
+	case french:
+		greeting = french_and_a_comma
+	case spanish:
+		greeting = spanish_hola_and_a_comma
+	}
+	return greeting + name
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
